bf1438b003b38c937424e89676f230d4118fab19
Fsoky <cyberuest0x12@gmail.com>
2022-03-06 17:05:33 +0600

    convert indent to spaces

--- dashboard/main.py
+++ dashboard/main.py
@@ -6,17 +6,17 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def index():
-	return redirect(Oauth2.login())
+    return redirect(Oauth2.login())
 
 
 @app.route("/login", methods=["GET"])
 async def login():
-	code = request.args.get("code")
-	access_token = await Oauth2.get_access_token(code)
+    code = request.args.get("code")
+    access_token = await Oauth2.get_access_token(code)
 
-	user = await Oauth2.get_user(access_token)
-	return f"{user['username']}#{user['discriminator']}"
+    user = await Oauth2.get_user(access_token)
+    return f"{user['username']}#{user['discriminator']}"
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
